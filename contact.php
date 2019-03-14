b5764f7b3e54ac43f339fbfb59a24775db722632
Cosimo Barbato <46413467+cosimobarbato@users.noreply.github.com>
2019-03-14 12:52:47 -0700

    Update articles, general styles, footer date

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php include "php/head.php"; ?>
+<section id="contact-hero" class="inner-hero section-white-border">
+  <div class="container">
+    <img src="images/contact.png" alt="The BNB Coach Contact Info">
+    <div class="about-content">
+      <h1>Contact Info</h1>
+      <p>cindy@thebnbcoach.com</p>
+      <p>Los Angeles, CA</p>
+    </div>  
+  </div>
+</section>
+
+  
+
+
+<?php include "php/contact-form.php" ?>
+<?php include "php/footer.php" ?>
